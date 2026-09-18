349384b2528e16d8f20acca1b1ebafa257be7da5
Naziya Parveen <166731080+Naziya007@users.noreply.github.com>
2026-09-18 11:57:57 +0530

    adding git ignore

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use('/api/faqs', require('./routes/faqs'));
 app.use('/api/blogs', require('./routes/blogs'));
 app.use('/api/case-studies', require('./routes/caseStudies'));
 app.use('/api/seo', require('./routes/seo'));
-app.use('/api/settings', require('./routes/settings'));
+app.use('/api/settings', require('./routes/settings')); 
 app.use('/api/upload', require('./routes/upload'));
 
 // Error handling middleware
